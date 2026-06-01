50a2b964271907198f84bae8c6c5afc4024b114d
latash-c <dr.latash@gmail.com>
2026-06-01 08:06:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-console.log("Lateesh JS loaded successfully!");
-
 function createElement(tag, text) {
     const el = document.createElement(tag);
     el.textContent = text;
     return el;
+}
+
+function render(element, target) {
+    target.appendChild(element);
 }
