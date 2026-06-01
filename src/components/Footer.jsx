8b37c128ec386686fa8f2155d49f70d76eeee9b0
latash-c <dr.latash@gmail.com>
2026-06-01 15:23:58 +0530

    Updated Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#f0f2f5",
+        padding: "15px",
+        textAlign: "center",
+        marginTop: "30px"
+      }}
+    >
+      © 2026 LateeshBook
+    </footer>
+  );
+}
 
+export default Footer;
